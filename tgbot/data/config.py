2fa48fcd4089f34484b36b3c4882d3688f192630
belaconic <asliddin.forwork@gmail.com>
2024-11-26 22:15:23 +0500

    Nothing

--- tgbot/data/config.py
+++ tgbot/data/config.py
@@ -9,6 +9,6 @@
 # DATABASE_URL: str = os.getenv('DATABASE_URL')
 
 
-botToken: str = "7846532519:AAGzL7gmjSlgJyiikqQL177ujUtPDoFrhDs"
-defaultNotionAPI: str = "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"
-databaseURL: str = "13ef5c09d39b807d810ce092e1d5aea7"
+botToken: str = ""
+defaultNotionAPI: str = ""
+databaseURL: str = ""
